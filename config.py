ada02c6b1676282990cb3afa9d84212e9fcb9f5b
Jakob Løver <loverjakob@gmail.com>
2020-03-01 04:09:43 +0100

    Config is checked for errors when loaded

--- config.py
+++ config.py
@@ -1,26 +1,79 @@
+import sys
+
 import yaml
+import importlib
 
 
 class Config:
     def __init__(self, path):
+        self.config_dict = dict()
+
+        self._load_config(path)
+        self._validate_config()
+
+    def _validate_config(self):
         try:
-            with open("config.yaml") as f:
+            # Parameters #
+            assert type(self.config_dict["parameters"]["image_duration"]) == int
+            assert type(self.config_dict["parameters"]["min_gif_duration"]) == int
+            assert type(self.config_dict["parameters"]["max_gif_duration"]) == int
+            assert type(self.config_dict["parameters"]["gif_iterations"]) == int
+            assert type(self.config_dict["parameters"]["time_delay_text"]) == int
+            assert type(self.config_dict["parameters"]["max_queue_size"]) == int
+            assert type(self.config_dict["parameters"]["media_filepath"]) == str
+            assert type(self.config_dict["parameters"]["log_filepath"]) == str
+            assert type(self.config_dict["parameters"]["min_aspect_ratio"]) == float
+            assert type(self.config_dict["parameters"]["max_aspect_ratio"]) == float
+            assert type(self.config_dict["parameters"]["title_font_size"]) == int
+
+            # Top level config values #
+            assert type(self.config_dict["downloaders"]) == dict
+            assert type(self.config_dict["weights"]) == dict
+            assert type(self.config_dict["schedule"]) == dict
+
+            # Check that all downloaders are configured correctly
+            # Example:
+            #
+            # reddit:
+            #   class: RedditDownloader
+            #   conf:
+            #       id:
+            #       secret:
+            for _downloader_name, _config in self.config_dict["downloaders"].items():
+
+                assert type(_downloader_name) == str
+                assert type(_config) == dict
+                assert type(_config["class"]) == str
+                assert type(_config["conf"]) == dict
+
+                # Check if we are able to create a class
+                _module = importlib.import_module("downloaders")
+                _class = getattr(_module, _config["class"])
+                instance = _class(_config)
+
+            # Check that the weighting section sums up to 1.0
+            _weights = 0
+            for k, v in self.config_dict["weights"].items():
+                _weights += v
+            assert _weights == 1
+
+        except AssertionError as e:
+            print("Key is of wrong type")
+            raise
+
+        except KeyError as e:
+            print(f'Error when accessing key "{e.args[0]}" in config')
+            raise
+
+    def _load_config(self, path):
+        try:
+            with open(path) as f:
                 configs = yaml.safe_load(f)
-                self._max_queue_size = configs["params"]["max_queue_size"]
-                self._image_duration = configs["params"]["image_duration"]
-                self._time_delay_text = configs["params"]["time_delay_text"]
-                self._media_filepath = configs["params"]["media_filepath"]
-                self._log_filepath = configs["params"]["log_filepath"]
-                self._title_font_size = configs["params"]["title_font_size"]
-                self._min_gif_duration = configs["params"]["min_gif_duration"]
-                self._max_gif_duration = configs["params"]["max_gif_duration"]
-                self._gif_iterations = configs["params"]["gif_iterations"]
+                self.config_dict = configs
+
         except FileNotFoundError:
             print("Config file does not exist")
-        except EnvironmentError:
-            print("Error when opening config. ", e)
-        except KeyError as e:
-            print("Could not find ", e)
+            raise
 
 
 # all parameters exist
